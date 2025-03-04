deeb0c06b12c0e1bd5fee5879e6952944c466d49
tiger <tigerpkchong@eprotel.com.hk>
2025-03-04 17:17:13 +0800

    udapte test

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddControllers()
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline. test
 app.UseCors(configurePolicy: policy =>
 {
     policy.SetIsOriginAllowed(origin =>
