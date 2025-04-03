16e76b48767e43cbe28523ebe8a1a1bc42b9eaf0
tiger <tigerpkchong@eprotel.com.hk>
2025-04-03 12:50:25 +0800

    update 2025-04-03 c

---
+++ Controllers/OutboundAttachmentController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Nodes;
+using WisePBX.NET8.Models.Wise;
+
+namespace WisePBX.NET8.Controllers
+{
+    [Route(template: "api")]
+    [ApiController]
+    public class OutboundAttachmentController : ControllerBase
+    {
+        private readonly string fileUploadPath;
+        
+        
+        public OutboundAttachmentController(IConfiguration iConfig, IWebHostEnvironment ienvironment)
+        {
+            fileUploadPath = iConfig.GetValue<string>("FileUploadPath") ?? "";
+            if (fileUploadPath == "")
+                fileUploadPath = ienvironment.ContentRootPath + "/Uploads";
+        }
+        [HttpPost]
+        [Route(template: "Outbound/UploadAttachment")]
+        public async Task<IActionResult> UploadAttachment(
+            [FromForm] int? caseNo, [FromForm] int agentId, [FromForm] int? campaignId, [FromForm] List<IFormFile> files)
+        {
+            if (files.Count == 0)
+                return Ok(new { result = WiseResult.Fail, details = "No File Upload." });
+            if (caseNo == null && campaignId == null)
+                return Ok(new { result = WiseResult.Fail, details = WiseError.InvalidParameters });
+
+            string webUrl = $"{Request.Scheme}://{Request.Host.Value.TrimEnd(':')}{Request.PathBase}";
+            string _tmpFolder = ((campaignId == null) ? "C" + caseNo : "O" + campaignId) + "-" + agentId + "-" + DateTime.Now.Ticks;
+            string _fillFolder = Path.Combine(fileUploadPath, _tmpFolder);
+            Directory.CreateDirectory(_fillFolder);
+            var data = new List<dynamic>();
+            foreach (var _file in files)
+            {
+                string _filePath = Path.Combine(_fillFolder, _file.FileName);
+                string _fullfilePath = Path.GetFullPath(_filePath);
+                if (_filePath.StartsWith(_fullfilePath, StringComparison.Ordinal))
+                {
+                    using (Stream fileStream = new FileStream(_fullfilePath, FileMode.Create))
+                    {
+                        await _file.CopyToAsync(fileStream);
+                    }
+
+
+                    data.Add(new
+                    {
+                        CreateDateTime = System.IO.File.GetCreationTime(_fullfilePath).ToString("s"),
+                        _file.FileName,
+                        FilePath = _filePath,
+                        _file.ContentType,
+                        FileUrl = webUrl + "/Uploads/" + _tmpFolder + "/" + _file.FileName
+
+                    });
+                }
+            }
+
+
+            return Ok(new { result = WiseResult.Success, data });
+        }
+
+        [HttpPost]
+        [Route(template: "Outbound/RemoveAttachment")]
+        public IActionResult RemoveAttachment([FromBody] JsonObject p)
+        {
+            List<string> _folders = [];
+            var files = p["files"]?.GetValue<List<string>>();
+            if (files != null)
+            {
+                foreach (string _filepath in files)
+                {
+                    System.IO.File.Delete(_filepath);
+                    string? _folder = Path.GetDirectoryName(_filepath);
+                    if (_folder != null && !_folders.Contains(_folder))
+                        _folders.Add(_folder);
+                }
+                foreach (string _folder in _folders)
+                {
+                    try
+                    {
+                        Directory.Delete(_folder, false);
+                    }
+                    catch (IOException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+
+
+                }
+            }
+
+            return Ok(new { result = WiseResult.Success });
+        }
+    }
+}
--- Controllers/OutboundController.cs
+++ Controllers/OutboundController.cs
@@ -7,7 +7,7 @@ using WisePBX.NET8.Models.Wise;
 
 namespace WisePBX.NET8.Controllers
 {
-    [Route("api/[controller]/[action]")]
+    [Route(template: "api")]
     [ApiController]
     public class OutboundController : ControllerBase
     {
@@ -23,6 +23,7 @@ namespace WisePBX.NET8.Controllers
             _wisedb = entities;
         }
         [HttpPost]
+        [Route(template: "Outbound/GetCallId")]
         public IActionResult GetCallId([FromBody] JsonObject p)
         {
             int callType = (p["callType"] == null) ? 0 : Convert.ToInt32(p["callType"]?.ToString());
@@ -46,7 +47,7 @@ namespace WisePBX.NET8.Controllers
         }
 
         [HttpPost]
-        [ActionName("CreateCaseId")]
+        [Route(template: "Outbound/CreateCaseId")]
         public IActionResult CreateCaseId([FromBody] JsonObject p)
         {
             int agentId = (p["agentId"] == null) ? -1 : Convert.ToInt32(p["agentId"]?.ToString());
@@ -61,80 +62,6 @@ namespace WisePBX.NET8.Controllers
             _wisedb.SaveChanges();
 
             return Ok(new { result = WiseResult.Success, data = _objCase.CaseId });
-        }
-        
-        [HttpPost]
-        public async Task<IActionResult> UploadAttachment(
-            [FromForm] int? caseNo, [FromForm] int agentId, [FromForm] int? campaignId, [FromForm] List<IFormFile> files)
-        {
-            if (files.Count == 0)
-                return Ok(new { result = WiseResult.Fail, details = "No File Upload." });
-            if(caseNo == null && campaignId==null)
-                return Ok(new { result = WiseResult.Fail, details = WiseError.InvalidParameters });
-
-            string webUrl = $"{Request.Scheme}://{Request.Host.Value.TrimEnd(':')}{Request.PathBase}";
-            string _tmpFolder = ((campaignId == null) ? "C" + caseNo : "O" + campaignId) + "-" + agentId + "-" + DateTime.Now.Ticks;
-            string _fillFolder = Path.Combine(fileUploadPath, _tmpFolder);
-            Directory.CreateDirectory(_fillFolder);
-            var data = new List<dynamic>();
-            foreach (var _file in files)
-            {
-                string _filePath = Path.Combine(_fillFolder, _file.FileName);
-                string _fullfilePath = Path.GetFullPath(_filePath);
-                if (_filePath.StartsWith(_fullfilePath, StringComparison.Ordinal))
-                {
-                    using (Stream fileStream = new FileStream(_fullfilePath, FileMode.Create))
-                    {
-                        await _file.CopyToAsync(fileStream);
-                    }
-
-
-                    data.Add(new
-                    {
-                        CreateDateTime = System.IO.File.GetCreationTime(_fullfilePath).ToString("s"),
-                        _file.FileName,
-                        FilePath = _filePath,
-                        _file.ContentType,
-                        FileUrl = webUrl + "/Uploads/" + _tmpFolder + "/" + _file.FileName
-
-                    });
-                }
-            }
-
-
-            return Ok(new { result = WiseResult.Success, data });
-        }
-
-        [HttpPost]
-        public IActionResult RemoveAttachment([FromBody] JsonObject p)
-        {
-            List<string> _folders = [];
-            var files = p["files"]?.GetValue<List<string>>();
-            if (files != null)
-            {
-                foreach (string _filepath in files)
-                {
-                    System.IO.File.Delete(_filepath);
-                    string? _folder = Path.GetDirectoryName(_filepath);
-                    if (_folder != null && !_folders.Contains(_folder))
-                            _folders.Add(_folder);
-                }
-                foreach (string _folder in _folders)
-                {
-                    try
-                    {
-                        Directory.Delete(_folder, false);
-                    }
-                    catch (IOException e)
-                    {
-                        Console.WriteLine(e.Message);
-                    }
-
-
-                }
-            }
-            
-            return Ok(new { result = WiseResult.Success });
         }
     }
 }
